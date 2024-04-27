2ca436d14e8be5227cb7c4f06c93fd3d2581d0cc
Victor Aguiar <134506027+victorcaua@users.noreply.github.com>
2024-04-27 01:11:05 -0300

    Add files via upload

---
+++ exercicios_aula/README.md
@@ -0,0 +1,1 @@
+Repositório compartilhado com o professor Lucas para a matéria de linguagem de programação Java do curso de Análise e Desenvolvimento de Sistemas do Centro Universitário Santa Cruz.
---
+++ exercicios_aula/src/aula09/Exercicio3.java
@@ -0,0 +1,22 @@
+package aula09;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe o nome de uma cidade");
+		String cidade = scan.next();
+		
+		if (cidade.equals("Curitiba")) {
+			System.out.println("Curitiba :)");
+		}
+		else {
+			System.out.println("Outra cidade");
+		}
+		scan.close();
+
+	}
+
+}
